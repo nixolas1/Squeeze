6724fab0743820292dc976413951b782107ab46f
Nicolas A. Tonne <nixatto@gmail.com>
2014-08-02 23:59:30 +0200

    Fixed design broken in last commit, properly implemented custom urls, optimized and cleaned

--- index.php
+++ index.php
@@ -1,57 +1,52 @@
 <?php
-    $short = $_GET['s'];
-    $long = $_GET['l'];
-    $list = json_decode(file_get_contents('url.json'), true);
-    $host = 'http://'.$_SERVER['HTTP_HOST'].'/+';
+$short = $_GET['s'];
+$long = $_GET['l'];
+$list = json_decode(file_get_contents('url.json'), true);
+$host = 'http://'.$_SERVER['HTTP_HOST'].'/+';                   // change '/+' to '/s?' if you dont use .htaccess redirect
+$len = 1;                                                       // initial length of new short urls
 
-    if(isset($short) and isset($long)){ //create custom short url
-        if(!isset($list[$short]))
-        file_put_contents("url.json", json_encode(array_merge($list, array ($short=>base64_decode($long)))));
-        else die("Custom url already taken! Try another one.");
+if(isset($short)){  //or is like /?s                                            // go to long url, given short
+    $long=$list[$short];
+    header("Location: ".$long);
+    exit;
+}
+else if(isset($long)){                                          // create short url
+    $long=base64_decode($long);
+    $split=explode(' ', $long);
+    if(sizeof($split)>1){
+        $short=$split[0]; $long=$split[1]; $listed=$list[$short];
+        if(isset($listed))die("URL '".$short."' taken.");
+    }else{
+        $hash=base64_encode(crc32($long));
+        $short = substr($hash, 0, $len);
+        while(isset($listed) and $long != $listed){
+            $short = substr($hash, 0, $len); $len++; $listed=$list[$short];
+        }
     }
-
-    else if(isset($short)){ //go to long url, given short
-        $long=$list[$short];
-        header("Location: ".$long);
-        exit;
-    }
-
-    else if(isset($long)){ //create short url given long
-        $short = substr(base64_encode(dechex(crc32($long))), 0, 5);
-        //$len=2;
-        //while(isset($list[$short]){ $short = substr(base64_encode(dechex(crc32($long))), 0, $len); $len++;}
-        $new = array ($short=>base64_decode($long));
-        file_put_contents("url.json", json_encode(array_merge($list,$new)));
-    }
+    file_put_contents("url.json", json_encode(array_merge($list,array ($short=>$long))));
+}
 ?>
 
 <html>
-<head>
-    <title>Squeeze - The world's simplest URL shortener</title>
-    <link rel="stylesheet" type="text/css" href="style.css" title="Default" />
-</head>
-<body>
-    <div class="top vcent">
-        <div class="centered">
+    <head>
+        <title>Squeeze</title>
+        <link rel="stylesheet" type="text/css" href="style.css" title="Default" />
+    </head>
+    <body>
+        <div class="top vcent centered">
             <div id="change" style="display:hidden;" class="small">
-                <?php if(isset($long)) echo '<a href="'.$host.$short. '">'.$host.$short.'</a>'; ?>
-
+                <?php if(isset($long)) echo '<a class="vcent" href="'.$host.$short. '">'.$host.$short.'</a>'; ?>
                 <form <?php if(isset($long)) echo 'style="display:none;"';?>
-                    name="ufo" action="" class="vcent" id="base" method="get" onsubmit="return submitform();">
+                      name="ufo" action="" class="" id="base" method="get" onsubmit="return submitform();">
                     <div class="face">&gt; &lt;</div>
                     <input id="longurl" name="l" type="text" class="innbox" />
                 </form>
             </div>
         </div>
-    </div>
-    
-    <script type="text/javascript">
-        function submitform() {
-                document.ufo.l.value = btoa(document.ufo.l.value);
-                document.ufo.submit();
-        }
-    </script>
-    
-</body>
+        <script type="text/javascript">
+            function submitform() {
+                document.ufo.l.value = btoa(document.ufo.l.value); document.ufo.submit();
+            }
+        </script>
+    </body>
 </html>
-
